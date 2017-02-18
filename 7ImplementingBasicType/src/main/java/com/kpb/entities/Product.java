943bd315496dd2806548ffe3f89ae1e4d8f82a02
KPB <15638181059@163.com>
2017-02-18 08:24:40 +0800

    例7：Custom BasicType implementation 例8 : Custom AbstractTypeDescriptor implementation 例9 : Register a Custom BasicType implementation 例10 :Custom BasicType mapping 例11 : Persisting the custom BasicType 例12 :Example 12. Persisting the custom BasicType

--- 7ImplementingBasicType/src/main/java/com/kpb/entities/Product.java
+++ 7ImplementingBasicType/src/main/java/com/kpb/entities/Product.java
@@ -10,6 +10,7 @@ import java.util.BitSet;
  * 创建人：贾林朋
  * 创建时间：2017 年 02 月 18 日
  * 联系邮箱：15638181059@163.com
+ * 描述：Example 10. Custom BasicType mapping
  */
 @Entity(name = "Product")
 public class Product {
