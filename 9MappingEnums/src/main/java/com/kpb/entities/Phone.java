072b2b49569517cf2d5dd1e9f50a8482722c493f
KPB <15638181059@163.com>
2017-02-18 10:22:07 +0800

    例17 ： PhoneType enumeration 例18: @Enumerated(ORDINAL) example 例19 : Persisting an entity with an @Enumerated(ORDINAL) mapping 例20 : @Enumerated(STRING) example 例21 ：ing an entity with an @Enumerated(STRING) mapping

--- 9MappingEnums/src/main/java/com/kpb/entities/Phone.java
+++ 9MappingEnums/src/main/java/com/kpb/entities/Phone.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
  * 创建人：贾林朋
  * 创建时间：2017 年 02 月 18 日
  * 联系邮箱：15638181059@163.com
- * 描述 ：
+ * 描述 ：Example 18. @Enumerated(ORDINAL) example
  */
 @Entity(name = "Phone")
 public class Phone {
